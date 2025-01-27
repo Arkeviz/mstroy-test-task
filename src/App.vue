b4535642fac1cc87c0a1e5e69eaab3db41c314df
Arkeviz <koposov@live.ru>
2025-01-27 17:17:21 +0300

    git init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main class="container">
+    <!-- Начало положено -->
+  </main>
+</template>
+
+<style scoped>
+  .container {
+    height: 100vh;
+    display: flex;
+    place-items: center;
+  }
+</style>
